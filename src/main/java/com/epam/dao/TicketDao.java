aa1a4f99fef187cbe2f71d2e712fb3f296876d74
Oleg Chorpita <chorpita@gmail.com>
2016-05-18 23:12:23 +0300

    impl dao: ticket

---
+++ src/main/java/com/epam/dao/TicketDao.java
@@ -0,0 +1,17 @@
+package com.epam.dao;
+
+import com.epam.model.Event;
+import com.epam.model.Ticket;
+import com.epam.model.User;
+
+import java.util.List;
+
+public interface TicketDao {
+    Ticket bookTicket(long userId, long eventId, int place, Ticket.Category category);
+
+    List<Ticket> getBookedTickets(User user, int pageSize, int pageNum);
+
+    List<Ticket> getBookedTickets(Event event, int pageSize, int pageNum);
+
+    public boolean cancelTicket(long ticketId);
+}
---
+++ src/main/java/com/epam/dao/impl/TicketDaoImpl.java
@@ -0,0 +1,34 @@
+package com.epam.dao.impl;
+
+import com.epam.dao.TicketDao;
+import com.epam.model.Event;
+import com.epam.model.Ticket;
+import com.epam.model.User;
+import com.epam.storage.EntityStorage;
+
+import java.util.List;
+
+public class TicketDaoImpl implements TicketDao {
+
+    EntityStorage storage;
+
+    @Override
+    public Ticket bookTicket(long userId, long eventId, int place, Ticket.Category category) {
+        return storage.bookTicket(userId, eventId, place, category);
+    }
+
+    @Override
+    public List<Ticket> getBookedTickets(User user, int pageSize, int pageNum) {
+        return storage.getBookedTickets(user, pageSize, pageNum);
+    }
+
+    @Override
+    public List<Ticket> getBookedTickets(Event event, int pageSize, int pageNum) {
+        return storage.getBookedTickets(event, pageSize, pageNum);
+    }
+
+    @Override
+    public boolean cancelTicket(long ticketId) {
+        return storage.cancelTicket(ticketId);
+    }
+}
