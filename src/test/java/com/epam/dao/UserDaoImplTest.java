924553e0402fed8a060b85603b6d59b3a07ebd60
Oleg Chorpita(epam) <chorpita@gmail.com>
2016-05-23 12:39:39 +0300

    fix test

--- src/test/java/com/epam/dao/UserDaoImplTest.java
+++ src/test/java/com/epam/dao/UserDaoImplTest.java
@@ -6,7 +6,13 @@ import com.epam.storage.EntityStorage;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.Profile;
 
+@Configuration
+@ImportResource("classpath:app-context.xml")
+@Profile("test")
 public class UserDaoImplTest {
     private User user;
 
