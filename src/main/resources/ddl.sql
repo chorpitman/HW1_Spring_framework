5101b83878872b9e14d5bbd45ecfbfbc5d07bb07
Oleg Chorpita <chorpita@gmail.com>
2016-06-02 23:36:10 +0300

    refactor ddl

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -1,38 +1,42 @@
 DROP DATABASE IF EXISTS booking_db;
 
-CREATE DATABASE IF NOT EXISTS booking_db DEFAULT CHARACTER SET utf8;
+CREATE DATABASE IF NOT EXISTS booking_db
+  DEFAULT CHARACTER SET utf8;
 
 USE booking_db;
 
 CREATE TABLE User (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR (50) NOT NULL UNIQUE,
+  id    INT         NOT NULL AUTO_INCREMENT,
+  name  VARCHAR(50) NOT NULL UNIQUE,
   email VARCHAR(50) NOT NULL UNIQUE,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE Event (
-  id INT NOT NULL AUTO_INCREMENT,
-  date DATETIME NOT NULL,
-  title VARCHAR (100) NOT NULL,
-  ticketPrice INT NOT NULL,
+  id          INT          NOT NULL AUTO_INCREMENT,
+  date        DATETIME     NOT NULL,
+  title       VARCHAR(100) NOT NULL,
+  ticketPrice INT          NOT NULL,
   PRIMARY KEY (id)
 );
 
- CREATE TABLE Ticket (
-  id INT NOT NULL AUTO_INCREMENT,
-  userId INT NOT NULL,
-  eventId INT NOT NULL,
-  category VARCHAR (50) NOT NULL UNIQUE,
+CREATE TABLE Ticket (
+  id       INT         NOT NULL AUTO_INCREMENT,
+  userId   INT         NOT NULL,
+  eventId  INT         NOT NULL,
+  category VARCHAR(50) NOT NULL UNIQUE,
   PRIMARY KEY (ID),
-  FOREIGN KEY (userId) REFERENCES USER (ID) on delete cascade,
-  FOREIGN KEY (eventId) REFERENCES EVENT (ID) on delete cascade
-  );
+  FOREIGN KEY (userId) REFERENCES USER (ID)
+    ON DELETE CASCADE,
+  FOREIGN KEY (eventId) REFERENCES EVENT (ID)
+    ON DELETE CASCADE
+);
 
- CREATE TABLE UserAccount (
-  id INT NOT NULL AUTO_INCREMENT,
-  userId INT NOT NULL,
+CREATE TABLE UserAccount (
+  id     INT           NOT NULL AUTO_INCREMENT,
+  userId INT           NOT NULL,
   amount DECIMAL(9, 2) NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY(userId) REFERENCES USER (id) on delete cascade
+  FOREIGN KEY (userId) REFERENCES USER (id)
+    ON DELETE CASCADE
 );
