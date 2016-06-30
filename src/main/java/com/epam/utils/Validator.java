bf6cf2775c07a094aa4e06ed0671448337a29255
Oleg Chorpita(mac) <chorpita@gmail.com>
2016-06-30 16:33:07 +0300

    refactor Validator. add private constr and modif final

--- src/main/java/com/epam/utils/Validator.java
+++ src/main/java/com/epam/utils/Validator.java
@@ -1,13 +1,17 @@
 package com.epam.utils;
 
-public class Validator {
+public final class Validator {
+
+    private Validator() {
+    }
+
     public static void checkNotNull(Object obj) {
         if (obj == null) {
             throw new IllegalArgumentException("wrong: parameter can not be null");
         }
     }
 
-    public static void checkExpression(boolean expression, String msg) {
+    static void checkExpression(boolean expression, String msg) {
         if (expression) {
             throw new IllegalArgumentException("wrong parameter: " + msg);
         }
