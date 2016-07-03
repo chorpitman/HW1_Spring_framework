978b1ac342c7514a25097f44994ab8db9a6337f9
Oleg Chorpita(mac) <chorpita@gmail.com>
2016-07-03 18:35:24 +0300

    refactor TicketDaoImpl. Add custom exception

--- src/main/java/com/epam/dao/impl/TicketDaoImpl.java
+++ src/main/java/com/epam/dao/impl/TicketDaoImpl.java
@@ -5,7 +5,10 @@ import com.epam.model.Event;
 import com.epam.model.Ticket;
 import com.epam.model.User;
 import com.epam.model.impl.TicketImpl;
+import com.epam.utils.TicketException;
+import com.epam.utils.Validator;
 import org.apache.log4j.Logger;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -19,7 +22,7 @@ import java.util.List;
 import java.util.Map;
 
 public class TicketDaoImpl implements TicketDao {
-    private static Logger logger = Logger.getLogger(TicketDaoImpl.class.getName());
+    private static Logger log = Logger.getLogger(TicketDaoImpl.class.getName());
 
     private final static String BOOKED_TICKETS_BY_ID = "SELECT * FROM Ticket WHERE id = :id";
     private final static String BOOK_TICKET = "INSERT INTO Ticket (userId, eventId, place, category) VALUES (:userId, :eventId, :place, :category)";
@@ -35,13 +38,20 @@ public class TicketDaoImpl implements TicketDao {
 
     @Override
     public Ticket bookedTicketById(long ticketId) {
-        logger.debug("bookedTicketById " + " ticketId:" + ticketId);
-        return jdbcTemplate.queryForObject(BOOKED_TICKETS_BY_ID, Collections.singletonMap("id", ticketId), new TicketMapper());
+        log.debug("bookedTicketById " + " ticketId:" + ticketId);
+        Ticket receivedTicket;
+        try {
+            receivedTicket = jdbcTemplate.queryForObject(BOOKED_TICKETS_BY_ID, Collections.singletonMap("id", ticketId), new TicketMapper());
+        } catch (DataAccessException e) {
+            throw new TicketException("ticket with [id]:" + ticketId + " doesn't exist");
+        }
+        Validator.checkNotNull(receivedTicket);
+        return receivedTicket;
     }
 
     @Override
     public Ticket bookTicket(long userId, long eventId, int place, Ticket.Category category) {
-        logger.debug("bookTicket " + " userId:" + userId + " eventId:" + eventId + " place" + place + " category" + category);
+        log.debug("bookTicket " + " userId:" + userId + " eventId:" + eventId + " place" + place + " category" + category);
         Map<String, Object> namedParameters = new HashMap<>();
         namedParameters.put("userId", userId);
         namedParameters.put("eventId", eventId);
@@ -50,40 +60,54 @@ public class TicketDaoImpl implements TicketDao {
         GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
         jdbcTemplate.update(BOOK_TICKET, new MapSqlParameterSource(namedParameters), keyHolder);
         int lastBookId = keyHolder.getKey().intValue();
-        return jdbcTemplate.queryForObject(BOOKED_TICKETS_BY_ID, Collections.singletonMap("id", lastBookId), new TicketMapper());
+        Ticket receivedTicket = jdbcTemplate.queryForObject(BOOKED_TICKETS_BY_ID, Collections.singletonMap("id", lastBookId), new TicketMapper());
+        Validator.checkNotNull(receivedTicket);
+        return receivedTicket;
     }
 
     @Override
     public List<Ticket> getBookedTickets(User user, int pageSize, int pageNum) {
-        logger.debug("getBookedTickets " + " user:" + " pageSize:" + pageSize + " pageNum:");
+        log.debug("getBookedTickets " + " user:" + " pageSize:" + pageSize + " pageNum:");
         int finish = (pageNum - 1) * pageSize;
         Map<String, Object> namedParameters = new HashMap<>();
         namedParameters.put("userId", user.getId());
         namedParameters.put("start", pageSize);
         namedParameters.put("finish", finish);
-        return jdbcTemplate.query(BOOKED_TICKETS_BY_USER, new MapSqlParameterSource(namedParameters), new TicketMapper());
+        List<Ticket> receivedTickets = jdbcTemplate.query(BOOKED_TICKETS_BY_USER, new MapSqlParameterSource(namedParameters), new TicketMapper());
+        if (receivedTickets.isEmpty()) {
+            throw new TicketException("when you have booked tickets error occurred" + user);
+        }
+        Validator.checkNotNull(receivedTickets);
+        return receivedTickets;
     }
 
     @Override
     public List<Ticket> getBookedTickets(Event event, int pageSize, int pageNum) {
-        logger.debug("getBookedTickets " + " event:" + " pageSize:" + " pageNum:" + pageNum);
+        log.debug("getBookedTickets " + " event:" + " pageSize:" + " pageNum:" + pageNum);
         int finish = (pageNum - 1) * pageSize;
         Map<String, Object> namedParameters = new HashMap<>();
         namedParameters.put("eventId", event.getId());
         namedParameters.put("start", pageSize);
         namedParameters.put("finish", finish);
-        return jdbcTemplate.query(BOOKED_TICKETS_BY_EVENT, new MapSqlParameterSource(namedParameters), new TicketMapper());
+        List<Ticket> receivedTickets = jdbcTemplate.query(BOOKED_TICKETS_BY_EVENT, new MapSqlParameterSource(namedParameters), new TicketMapper());
+        if (receivedTickets.isEmpty()) {
+            throw new TicketException("when you have booked tickets error occurred: " + event);
+        }
+        Validator.checkNotNull(receivedTickets);
+        return receivedTickets;
     }
 
     @Override
     public boolean cancelTicket(long ticketId) {
-        logger.debug("cancelTicket " + " ticketId");
+        log.debug("cancelTicket " + " ticketId");
+        bookedTicketById(ticketId);
         return jdbcTemplate.update(CANCEL_TICKET, Collections.singletonMap("id", ticketId)) > 0;
     }
 
     private static final class TicketMapper implements RowMapper<Ticket> {
         @Override
         public Ticket mapRow(ResultSet resultSet, int i) throws SQLException {
+            log.debug("mapRow:" + resultSet + " " + i);
             Ticket ticket = new TicketImpl();
             ticket.setId(resultSet.getLong("id"));
             ticket.setUserId(resultSet.getLong("userId"));
