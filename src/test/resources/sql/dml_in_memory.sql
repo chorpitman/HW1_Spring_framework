ed0a206a81658f5cd79c29b92618cd223b5b3829
Oleg Chorpita(mac) <chorpita@gmail.com>
2016-07-04 12:53:40 +0300

    add properties for sonar

--- src/test/resources/sql/dml_in_memory.sql
+++ src/test/resources/sql/dml_in_memory.sql
@@ -1,25 +1,25 @@
-insert into User (name, email) values ('Joe', 'joe@i.ua');
-insert into User (name, email) values ('Jack', 'jack@i.ua');
-insert into User (name, email) values ('John', 'John@i.ua');
-insert into User (name, email) values ('John', 'John1@i.ua');
-insert into User (name, email) values ('John', 'John2@i.ua');
-insert into User (name, email) values ('John', 'John3@i.ua');
-insert into User (name, email) values ('John', 'John5@i.ua');
+INSERT INTO User (name, email) VALUES ('Joe', 'joe@i.ua');
+INSERT INTO User (name, email) VALUES ('Jack', 'jack@i.ua');
+INSERT INTO User (name, email) VALUES ('John', 'John@i.ua');
+INSERT INTO User (name, email) VALUES ('John', 'John1@i.ua');
+INSERT INTO User (name, email) VALUES ('John', 'John2@i.ua');
+INSERT INTO User (name, email) VALUES ('John', 'John3@i.ua');
+INSERT INTO User (name, email) VALUES ('John', 'John5@i.ua');
 
-insert into Event (date, title, ticketPrice) values ('2016-05-01', 'VELODAY', 101);
-insert into Event (date, title, ticketPrice) values ('2016-06-01', 'VELODAY', 201);
-insert into Event (date, title, ticketPrice) values ('2016-07-01', 'VELODAY', 301);
-insert into Event (date, title, ticketPrice) values ('2016-08-01', 'BOX', 401);
+INSERT INTO Event (date, title, ticketPrice) VALUES ('2016-05-01', 'VELODAY', 101);
+INSERT INTO Event (date, title, ticketPrice) VALUES ('2016-06-01', 'VELODAY', 201);
+INSERT INTO Event (date, title, ticketPrice) VALUES ('2016-07-01', 'VELODAY', 301);
+INSERT INTO Event (date, title, ticketPrice) VALUES ('2016-08-01', 'BOX', 401);
 
-insert into Ticket (userId, eventId, place, category) values (1, 1, 10, 'STANDARD');
-insert into Ticket (userId, eventId, place, category) values (2, 1, 20, 'PREMIUM');
-insert into Ticket (userId, eventId, place, category) values (3, 2, 30, 'BAR');
-insert into Ticket (userId, eventId, place, category) values (1, 2, 40, 'BAR');
+INSERT INTO Ticket (userId, eventId, place, category) VALUES (1, 1, 10, 'STANDARD');
+INSERT INTO Ticket (userId, eventId, place, category) VALUES (2, 1, 20, 'PREMIUM');
+INSERT INTO Ticket (userId, eventId, place, category) VALUES (3, 2, 30, 'BAR');
+INSERT INTO Ticket (userId, eventId, place, category) VALUES (1, 2, 40, 'BAR');
 
-insert into UserAccount (userId, amount) values (1, 50);
-insert into UserAccount (userId, amount) values (2, 0);
-insert into UserAccount (userId, amount) values (3, 0);
-insert into UserAccount (userId, amount) values (4, 10);
-insert into UserAccount (userId, amount) values (5, 5);
-insert into UserAccount (userId, amount) values (6, 0);
-insert into UserAccount (userId, amount) values (7, 0);
+INSERT INTO UserAccount (userId, amount) VALUES (1, 50);
+INSERT INTO UserAccount (userId, amount) VALUES (2, 0);
+INSERT INTO UserAccount (userId, amount) VALUES (3, 0);
+INSERT INTO UserAccount (userId, amount) VALUES (4, 10);
+INSERT INTO UserAccount (userId, amount) VALUES (5, 5);
+INSERT INTO UserAccount (userId, amount) VALUES (6, 0);
+INSERT INTO UserAccount (userId, amount) VALUES (7, 0);
