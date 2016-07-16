318f168eca96d008282ed752bccf6d8f29db1f80
Oleg Chorpita(mac) <chorpita@gmail.com>
2016-07-16 23:27:41 +0300

    refactor event controller

--- src/main/java/com/epam/controller/EventController.java
+++ src/main/java/com/epam/controller/EventController.java
@@ -2,16 +2,21 @@ package com.epam.controller;
 
 import com.epam.facade.BookingFacade;
 import com.epam.model.Event;
+import com.epam.model.impl.EventImpl;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Date;
 import java.util.List;
 
-@RequestMapping(value = {"/event"})
+@RequestMapping(value = {"/event"}, consumes ="application/json")
 @Controller
 public class EventController {
     private static Logger log = Logger.getLogger(EventController.class.getName());
@@ -19,36 +24,59 @@ public class EventController {
     @Autowired
     BookingFacade facade;
 
-    @RequestMapping(value = "/get/", method = RequestMethod.GET)
-    public Event getEventById(long eventId) {
-        log.info("getEventById: " + eventId);
-        return facade.getEventById(eventId);
+    @RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
+    @ResponseBody //Аннотация @ResponseBody
+    // сообщает фреймворку Spring, что возвращаемый
+    // объект следует отправить клиенту как ресурс, преобразовав
+    // его в некоторый формат, доступный для клиента.
+    // ресурс должен быть преобразован в формат в соответствии с содержимым
+    // заголовка Accept. Если в запросе отсутствует заголовок Accept,
+    // тогда предполагается, что клиент способен принимать ресурсы в любом формате.
+    public Event getEventById(@PathVariable("id") long id) {
+        log.info("getEventById: " + id);
+        return facade.getEventById(id);
     }
 
-    @RequestMapping(value = "/title/", method = RequestMethod.GET)
-    public List<Event> getEventsByTitle(String title, int pageSize, int pageNum) {
-        log.info("getEventsByTitle: " + title + pageSize + pageNum);
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    @ResponseBody
+    public Event createEvent(@RequestBody EventImpl event) {
+        log.info("createEvent" + event);
+        facade.createEvent(event);
+        return facade.createEvent(event);
+        // для преобразования данных HTTP-запроса в Java- объекты, передаваемые
+        // методам-обработчикам контроллера, можно воспользоваться
+        // новой аннотацией @RequestBody
+    }
+
+    @RequestMapping(value = "/title", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Event> getEventsByTitle(@RequestParam String title,
+                                        @RequestParam("pageSize") int pageSize,
+                                        @RequestParam("pageNum") int pageNum) {
+        log.info("getEventsByTitle: " + title + ";" + pageSize + ";" + pageNum);
         return facade.getEventsByTitle(title, pageSize, pageNum);
     }
 
-    @RequestMapping(value = "/day/", method = RequestMethod.GET)
-    public List<Event> getEventsForDay(Date day, int pageSize, int pageNum) {
+    @RequestMapping(value = "/day", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Event> getEventsForDay(@RequestParam("day") Date day,
+                                       @RequestParam("pageSize") int pageSize,
+                                       @RequestParam("pageNum") int pageNum) {
+        log.info("getEventsForDay: " + day + ";" + pageSize + ";" + pageNum);
         return facade.getEventsForDay(day, pageSize, pageNum);
     }
 
-    @RequestMapping(value = "/create/", method = RequestMethod.POST)
-    public Event createEvent(Event event) {
-        return facade.createEvent(event);
-    }
-
-    @RequestMapping(value = "/update/", method = RequestMethod.PUT)
-    public Event updateEvent(Event event) {
+    @RequestMapping(value = "/update", method = RequestMethod.PUT)
+    @ResponseBody
+    public Event updateEvent(@RequestBody EventImpl event) {
+        log.info("updateEvent: " + event);
         return facade.updateEvent(event);
     }
 
-    @RequestMapping(value = "/delete/", method = RequestMethod.GET)
-    // TODO: 14.07.16 think about request method
-    public boolean deleteEvent(long eventId) {
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    @ResponseBody
+    public boolean deleteEvent(@RequestParam("id") long eventId) {
+        log.info("deleteEvent: " + eventId);
         return facade.deleteEvent(eventId);
     }
 }
