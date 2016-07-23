a0ba28270ed04efe89e21f733aff4ec70c18627f
Oleg Chorpita(mac) <chorpita@gmail.com>
2016-07-23 22:55:03 +0300

    Start impl test controller

--- src/test/java/com/epam/controller/EventControllerTest.java
+++ src/test/java/com/epam/controller/EventControllerTest.java
@@ -2,50 +2,67 @@ package com.epam.controller;
 
 import com.epam.model.Event;
 import com.epam.model.impl.EventImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@Configuration
-@ImportResource("classpath:app-context.xml")
-@Profile("test")
 @RunWith(MockitoJUnitRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = "classpath:root-context.xml")
+@Sql(scripts = {"classpath:sql/drop.sql", "classpath:sql/ddl_in_memory.sql", "classpath:sql/dml_in_memory.sql"})
+
+
+//@Configuration
+//@ImportResource("classpath:app-context.xml")
+//@Profile("test")
 public class EventControllerTest {
 
-    @InjectMocks
-    EventImpl event;
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private MockMvc mockMvc;
 
     @Mock
     EventController mockEventController;
 
-    @Test
-    public void testGetEventById() {
-        when(mockEventController.getEventById(event.getId())).thenReturn(event);
-        mockEventController.getEventById(event.getId());
-        verify(mockEventController).getEventById(event.getId());
+    Event event;
+
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
     }
 
     @Test
-    public void testGetEventsByTitle()  {
-        event = new EventImpl(1L, "title", new Date(), BigDecimal.TEN);
-        List<Event> eventList = Arrays.asList(event);
-        when(mockEventController.getEventsByTitle(event.getTitle(), 1, 1)).thenReturn(eventList);
-        assertEquals(eventList, mockEventController.getEventsByTitle(event.getTitle(), 1, 1));
-        verify(mockEventController).getEventsByTitle(event.getTitle(), 1, 1);
+    public void testGetEventById() {
+    }
+
+    @Test
+    public void testGetEventsByTitle() {
     }
 
     @Test
@@ -55,7 +72,10 @@ public class EventControllerTest {
 
     @Test
     public void testCreateEvent() throws Exception {
-
+        event = new EventImpl(1L, "ACDC", new Date(), new BigDecimal(20));
+        mockMvc.perform(MockMvcRequestBuilders.post("/event/create/").content(objectMapper.writeValueAsString(event))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 
     @Test
